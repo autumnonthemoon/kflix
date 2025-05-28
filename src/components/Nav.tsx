e716fe35495337d460c69427189059ef3d0250fd
Thu H <poemautumnhh@gmail.com>
2025-05-28 14:31:54 +0200

    some mobile fixes +

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,7 @@ import { JSX, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import './Nav.scss'
 import { useTheme } from "../context/ThemeContext";
+import useMediaQuery from "./MediaQuery";
 
 interface NavPropsTypes {
     show: boolean
@@ -12,16 +13,19 @@ export default function Nav(props: NavPropsTypes): JSX.Element {
     const location = useLocation()
     const { theme, setTheme } = useTheme()
     const [showMenu, setShowMenu] = useState<boolean>(false)
+    const isMobile = useMediaQuery('(max-width: 768px)')
 
     const toggleMenu = () => {
-        setShowMenu((prev) => !prev)
+        if(isMobile){
+            setShowMenu((prev) => !prev)
+        }
     }
 
     return <nav className={`nav ${(show || location.pathname.indexOf("details") >= 0 || location.pathname.indexOf("favorites") >= 0 || location.pathname.indexOf("search") >= 0) && "nav-bg"} ${showMenu ? 'show-mobile-menu' : ''}`}>
         <ul>
-            <li  onClick={toggleMenu}><Link to="/"><h3 className="logo">KFlix &#x2764;</h3></Link></li>
-            <li  onClick={toggleMenu}><Link to="/favorites">Favorites</Link></li>
-            <li  onClick={toggleMenu}><Link to="/search"><div className="search-icon"></div></Link></li>
+            <li onClick={showMenu ? toggleMenu : undefined}><Link to="/"><h3 className="logo">KFlix &#x2764;</h3></Link></li>
+            <li onClick={showMenu ? toggleMenu : undefined}><Link to="/favorites">Favorites</Link></li>
+            <li onClick={showMenu ? toggleMenu : undefined}><Link to="/search"><div className="search-icon"></div></Link></li>
             <li className="close"><div className="close-button" onClick={() => setShowMenu((prev) => !prev)}></div></li>
         </ul>
 
